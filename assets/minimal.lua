b142fa7a3b333a975fc10335adaa075f3c8d21af
Isak Samsten <isak@samsten.se>
2025-10-02 14:04:59 +0200

    Move sia setup before mini.nvim tests

--- assets/minimal.lua
+++ assets/minimal.lua
@@ -1,7 +1,7 @@
 vim.cmd([[let &rtp.=','.getcwd()]])
 
 if #vim.api.nvim_list_uis() == 0 then
+  require("sia").setup()
   vim.cmd("set rtp+=deps/mini.nvim")
   require("mini.test").setup()
-  require("sia").setup()
 end
