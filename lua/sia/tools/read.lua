73a29be4421eaf352c7471e1c92e5b994414c691
Isak Samsten <isak@samsten.se>
2025-10-13 11:19:21 +0200

    Emphasize recommendation to read whole file in prompt

--- lua/sia/tools/read.lua
+++ lua/sia/tools/read.lua
@@ -10,8 +10,8 @@ return tool_utils.new_tool({
   message = "Reading file contents...",
   system_prompt = [[Reads a file from the local filesystem. By default, it reads up to
 2000 lines starting from the beginning of the file. You can optionally specify a line
-offset and limit (especially handy for long files), but it`s recommended to read the
-whole file by not providing these parameters. Any lines longer than 2000 characters
+offset and limit (especially handy for long files), but it`s RECOMMENDED TO READ THE
+WHOLE FILE by not providing these parameters. Any lines longer than 2000 characters
 will be truncated.]],
   description = [[Reads a file from the local filesystem.]],
   parameters = {
